32cae6c3d3e4d166a770ab1491cd7382d400caea
Andrei Kniazev <knezewski@yandex.ru>
2020-08-25 00:45:18 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SearchMovies from './SearchMovies';
+import './index.css';
+
+function App() {
+
+  return (
+    <div className="container">
+      <h1 className="title">React Movie Search</h1>
+      <SearchMovies />
+  </div>
+  );
+}
+
+export default App;
